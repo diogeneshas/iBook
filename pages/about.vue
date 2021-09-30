1e28dac5952570e1f88ed75d4aad78a3a402ab87
diogeneshas <diogenes.henrique.br@gmail.com>
2021-09-30 07:43:00 -0300

    Layout iBook

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+vsbas<template>
+    <div>
+        <h1>Sobre</h1>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        layout: 'ibook'
+    })
+</script>
+
+<style scoped>
+
+</style>
