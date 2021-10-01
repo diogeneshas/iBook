62f5e309410eeb1504314965723d638130e5a777
diogeneshas <diogenes.henrique.br@gmail.com>
2021-10-01 07:23:59 -0300

    Entendendo a sintaxe

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="box">
-    <HomeTemplate />
+    <!-- <HomeTemplate /> -->
+    <h1>{{ $count }}</h1>
+    <button @click="increment">incrementar</button>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
+  import { books } from '@/store'
 
   export default Vue.extend({
-    layout: 'ibook'
+    layout: 'ibook',
+    computed: {
+      $count() {
+        return books.$count
+      }
+    },
+    methods: {
+      increment() {
+        books.increment()
+      }
+    }
   })
 </script>
 
--- store/books.ts
+++ store/books.ts
@@ -2,5 +2,19 @@ import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
 
 @Module({ name: 'books', stateFactory: true, namespaced: true })
 export default class Books extends VuexModule {
-  books = ['livro 1', 'livro 2']
+  private count = 0
+
+  get $count() {
+    return this.count
+  }
+
+  @Mutation
+  INCREMENT(number: number) {
+    this.count += number
+  }
+
+  @Action
+  increment() {
+    this.context.commit('INCREMENT', 5)
+  }
 }
